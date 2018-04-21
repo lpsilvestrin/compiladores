9699923c0db6f9812a20a010e28c24145c42310d
mzboito <mzboito@gmail.com>
2018-04-21 22:10:27 +0200

    astree with create function

--- astree.c
+++ astree.c
@@ -5,8 +5,14 @@
 #include "astree.h"
 
 
-
-
-void initASTree(){
-    
+ASTree* astree_create(int type, hashNode *id, ASTree *offspring_0, ASTree *offspring_1, ASTree *offspring_2, ASTree *offspring_3){
+    ASTree *node = (ASTree*)malloc(sizeof(ASTree));
+    node->type = type;
+    node->id = id;
+    node->offspring[0] = offspring_0;
+    node->offspring[1] = offspring_1;
+    node->offspring[2] = offspring_2;
+    node->offspring[3] = offspring_3;
+    return node;
 }
+
--- astree.h
+++ astree.h
@@ -4,14 +4,17 @@
 
 #define MAX_OFFSPRING 4 //suggested by our teacher
 
+/*here we will put a lot of defines with values for representing the possible types inside the AST*/
+
+
 #include "hashtable.h"
 
 typedef struct astree {
     int type; 
-    hashNode * symbol; //pointer for the info inside our hash
+    hashNode *id; //pointer for the info inside our hash
     struct astree *offspring[MAX_OFFSPRING];
 } ASTree;
 
-void initASTree();
+ASTree* astree_create(int type, hashNode *id, ASTree *offspring_0, ASTree *offspring_1, ASTree *offspring_2, ASTree *offspring_3);
 
 #endif
