2456a34e45319c016ceef8b396d62fc29fb09dee
lpsilvestrin <luisp.silvestrin@gmail.com>
2018-04-22 17:31:46 -0300

    refactoring decompile function

--- astree.c
+++ astree.c
@@ -29,50 +29,58 @@ void print_astnode(ASTree* node, int level) {
 	}
 }
 
-void decompile_tree(ASTree* tree) {
-	switch(tree->type) {
-		case AST_GLOBAL_VAR_DEF:
-		case AST_VECTOR_DEF:
-		case AST_INT:
-		case AST_FUNCTION_DEF:
-		case AST_HEADER:
-		case AST_DEF_PARAM:
-		case AST_DEF_PARAM_T:
-		case AST_COMMANDS_L:
-		case AST_VECTOR_AS:
-		case AST_VAR_AS:
-		case AST_READ:
-		case AST_PRINT:
-		case AST_NOT_EXP:
-		case AST_NEG_EXP:
-		case AST_PAR_EXP:
-		case AST_PLUS_EXP:
-		case AST_MINUS_EXP:
-		case AST_MUL_EXP:
-		case AST_DIV_EXP:
-		case AST_LESS_EXP:
-		case AST_GREAT_EXP:
-		case AST_LE_EXP:
-		case AST_GE_EXP:
-		case AST_EQ_EXP:
-		case AST_NE_EXP:
-		case AST_AND_EXP:
-		case AST_OR_EXP:
-		case AST_ID:
-		case AST_ID_POINTER:
-		case AST_ID_ADDRESS:
-		case AST_VECTOR:
-		case AST_FUNCTION:
-		case AST_PARAM:
-		case AST_IF:
-		case AST_IFELSE:
-		case AST_FOR:
-		case AST_WHILE:
-		case AST_REAL:
-		case AST_CHAR:
-		case AST_INIT_VALUES:
-			printf(
-		
-		default: 
+int decompile_tree(ASTree* tree, FILE *prog) {
+	if (tree == NULL) {
+		return 0;
 	}
+	switch(tree->type) {
+		case AST_INITIAL:
+			decompile_tree(tree->offspring[0], prog);
+			fprintf(prog, ' ');	
+			decompile_tree(tree->offspring[1], prog);
+			break;
+		case AST_GLOBAL_VAR_DEF:
+			decompile_tree(tree->offspring[0], prog);
+		case AST_VECTOR_DEF: break;
+		case AST_INT: break;
+		case AST_FUNCTION_DEF: break;
+		case AST_HEADER: break;
+		case AST_DEF_PARAM: break;
+		case AST_DEF_PARAM_T: break;
+		case AST_COMMANDS_L: break;
+		case AST_VECTOR_AS: break;
+		case AST_VAR_AS: break;
+		case AST_READ: break;
+		case AST_PRINT: break;
+		case AST_NOT_EXP: break;
+		case AST_NEG_EXP: break;
+		case AST_PAR_EXP: break;
+		case AST_PLUS_EXP: break;
+		case AST_MINUS_EXP: break;
+		case AST_MUL_EXP: break;
+		case AST_DIV_EXP: break;
+		case AST_LESS_EXP: break;
+		case AST_GREAT_EXP: break;
+		case AST_LE_EXP: break;
+		case AST_GE_EXP: break;
+		case AST_EQ_EXP: break;
+		case AST_NE_EXP: break;
+		case AST_AND_EXP: break;
+		case AST_OR_EXP: break;
+		case AST_ID: break;
+		case AST_ID_POINTER: break;
+		case AST_ID_ADDRESS: break;
+		case AST_VECTOR: break;
+		case AST_FUNCTION: break;
+		case AST_PARAM: break;
+		case AST_IF: break;
+		case AST_IFELSE: break;
+		case AST_FOR: break;
+		case AST_WHILE: break;
+		case AST_REAL: break;
+		case AST_CHAR: break;
+		case AST_INIT_VALUES: break;
+		default: break;
+	}
+	return 0;
 }
