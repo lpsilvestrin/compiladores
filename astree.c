4dfca095c9b899736de633b2696e0a7ac2a46f3d
lpsilvestrin <luisp.silvestrin@gmail.com>
2018-04-26 23:45:48 -0300

    remove reference from ast_ids

--- astree.c
+++ astree.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "astree.h"
-#include "ast_ids.h"
+//#include "ast_ids.h"
 
 void print_binary_op(char* op, ASTree* n1, ASTree *n2, FILE* prog) {
 	decompile_tree(n1,prog);
