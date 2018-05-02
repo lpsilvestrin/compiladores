901133a8dacaf127c32b221143ec3500e638cc00
lpsilvestrin <luisp.silvestrin@gmail.com>
2018-05-01 22:13:26 -0300

    merging

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+DIROUT=out
+DIROUT2=out_cpy
+
 make clean
 make
+mkdir -p $DIROUT
+mkdir -p $DIROUT2
 
 for f in tests/*; do 
+	fn=$(basename $f)
 	echo $f;
-	./etapa3 $f $f".out"
-	
+	./etapa3 $f $DIROUT/$fn".out"	
+done
+
+
+for f in $DIROUT/*; do 
+	fn=$(basename $f)
+	echo $f;
+	./etapa3 $f $DIROUT2/$fn
+done
+
+
+for f in $DIROUT/*; do 
+	fn=$(basename $f)
+	echo $f;
+	diff $f $DIROUT2/$fn
 done
