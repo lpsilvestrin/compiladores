205fd7714dac4b575c5743061c467930ba11fee9
joao gmail <joao@gmail.com>
2018-05-21 17:03:28 -0300

    refactor to accept call without passing output file

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ extern hashTable *SymbolsTable;
 extern int _SEMANTIC_ERROR;
 
 int main(int argc, char** argv) {
-	if (argc < 3) {
-		printf("Usage: etapa4 <input_file> <output_file>\n");
+	if (argc < 2) {
+		printf("Usage: etapa4 <input_file> [output_file]\n");
 		exit(1);
 	}
 	if((yyin = fopen(argv[1], "r"))==NULL) {
@@ -22,12 +22,15 @@ int main(int argc, char** argv) {
 	}
 
 	FILE* f;
-	if((f = fopen(argv[2],"w"))==NULL) {
-		printf("Problem reading the file %s\n", argv[2]);
-		exit(1);
+	if(argc > 2) { 
+		if ((f = fopen(argv[2],"w"))==NULL) {
+			printf("Problem reading the file %s\n", argv[2]);
+			exit(1);
+			setOutput(f); //send the pointer to ther parser
+		}
 	}
 	initMe();
-	setOutput(f); //send the pointer to ther parser
+
 	if(yyin){
 		yyparse();
 		if (_SEMANTIC_ERROR == 1) {
