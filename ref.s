1e7df63c8cd9d6e9a61eb4eefafd96e2a2b5615b
lpsilvestrin <luisp.silvestrin@gmail.com>
2018-06-27 22:20:06 -0300

    function body example

--- ref.s
+++ ref.s
@@ -1,10 +1,21 @@
+// define variables
 	.data
 x:
 	.long	42
 y:
 	.long	36
-z:
+_z:
 	.long	0
+// function declaration
+foo:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	movq	%rsp, %rbp
+	// function body
+	popq	%rbp
+	ret
+	.cfi_endproc
 	.section	.rodata
 .LC0:
 	.string	"test"
@@ -14,14 +25,21 @@ z:
 	.string	"%d"
 	.globl	main
 main:
-.LFB0:
+.LFB1:
 	.cfi_startproc
 	pushq	%rbp
 	movq	%rsp, %rbp
 	movq	$.LC0, %rsi
 	movl	$.LC1, %edi
 	call	printf
-	movq	y(%rip), %rsi
+
+//printf
+// variable "label" access: label(%rip)
+	movl	$0, %eax
+	addl	x(%rip), %eax 
+	addl	y(%rip), %eax
+	movl	%eax, _z(%rip)
+	movq	_z(%rip), %rsi
 	movl	$.LC2, %edi
 	call	printf
 	
