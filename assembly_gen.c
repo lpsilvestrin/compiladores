f9e142cc936da79e80ffb7944e493ca0233f2997
lpsilvestrin <luisp.silvestrin@gmail.com>
2018-06-28 00:12:21 -0300

    fun begin & fun end

--- assembly_gen.c
+++ assembly_gen.c
@@ -6,11 +6,23 @@ void tac_translate(TAC* tac, FILE* fout) {
 		fprintf(fout, "%s:\n", tac->result->id);
 		fprintf(fout, "\t.long\t%s\n",tac->op1->id);
 		break;
+	case TAC_FUN_BEGIN:
+		fprintf(fout, "\t.globl %s\n", tac->result->id);
+		fprintf(fout, "%s:\n", tac->result->id);
+		fprintf(fout, "\t.cfi_startproc\n\tpushq\t%%rbp\n");
+		fprintf(fout, "\tmovq\t%%rsp, %%rbp\n");
+		break;
+	case TAC_FUN_END:
+		fprintf(fout, "\tpopq\t%%rbp\n");
+		fprintf(fout, "\tret\n");
+		fprintf(fout, "\t.cfi_endproc\n");
+		break;	
 	}	
 }
 
 int gen_assembly(TAC* tac_list, FILE *fout) {
 	TAC* tmp = tac_list;
+	fprintf(fout, ".data");
 	for(; tmp != NULL; tmp = tmp->next) {
 		tac_translate(tmp, fout);
 	}
